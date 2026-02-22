50fd35377de11e9b27235960a0481876a235a253
unknown <frontendcnn@gmail.com>
2026-02-22 16:10:18 -0300

    Reformatando as pastas

--- BackendTidb/src/config/database.js
+++ BackendTidb/src/config/database.js
@@ -1,29 +1,14 @@
-// import mysql from 'mysql2/promise';
-
-// const pool = mysql.createPool({
-//     port: 4000, // padrão do TiDB
-//     host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',
-//     user: 'HMzW9A9LdZtQp9s.root',
-//     password: 'LeliAv4QfhYagaTZ',
-//     database: 'test',
-//     ssl: {
-//         minVersion: 'TLSv1.2',
-//         rejectUnauthorized: false
-//     }
-// });
-
-// console.log('Pool de conexão criada');
-
-// export default pool;
-
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = await mysql.createPool({
-    host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',
-    port: 4000,
-    user: 'HMzW9A9LdZtQp9s.root',
-    password: 'LeliAv4QfhYagaTZ',
-    database: 'test',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true
